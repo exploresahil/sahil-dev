dd276bf3dc131abb52bd5f21e00a90accab819db
Sahil Satpute <explore.sahil1@gmail.com>
2024-05-11 13:20:11 +0530

    uploaded code

---
+++ app/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+import About from "@/components/home/about/About";
+import Hero from "@/components/home/hero/Hero";
+import { useLayoutEffect } from "react";
+import Lenis from "lenis";
+
+export default function Home() {
+  useLayoutEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
+  return (
+    <>
+      <Hero />
+      <About />
+    </>
+  );
+}
---
+++ database/header.ts
@@ -0,0 +1,9 @@
+const menuItem = [
+  { title: "Website Section", link: "/" },
+  { title: "Magazine", link: "/" },
+  { title: "Design", link: "/" },
+  { title: "Websites", link: "/" },
+  { title: "Api", link: "/" },
+];
+
+export default menuItem;
