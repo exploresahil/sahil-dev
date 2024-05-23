f01545cf2ae7363c6b8e97f01c3c37369d005203
Sahil Satpute <explore.sahil1@gmail.com>
2024-05-23 21:28:53 +0530

    deleted code

--- database/header.ts
+++
@@ -1,9 +0,0 @@
-const menuItem = [
-  { title: "Website Section", link: "/" },
-  { title: "Magazine", link: "/" },
-  { title: "Design", link: "/" },
-  { title: "Websites", link: "/" },
-  { title: "Api", link: "/" },
-];
-
-export default menuItem;
--- utils/image-utils.ts
+++
@@ -1,6 +0,0 @@
-const ImageSize = {
-  bannerSizes: "(max-width: 768px) 600px, (max-width: 1200px) 800px, 1920px",
-  cardSize: "(max-width: 768px) 500px, (max-width: 1200px) 600px, 700px",
-};
-
-export default ImageSize;
