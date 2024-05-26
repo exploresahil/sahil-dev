a5385f1d6ea35fc980a1fc66a64eceff93165299
Sahil Satpute <explore.sahil1@gmail.com>
2024-05-27 00:05:03 +0530

    Update route.ts

--- app/api/getWebsiteSections/route.ts
+++ app/api/getWebsiteSections/route.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import navItems from "@/components/projects/website-sections/header/dropdown/db";
+import navItems from "@/components/projects/db";
 import { NextResponse } from "next/server";
 
 export const GET = (req: NextApiRequest, res: NextApiResponse) => {
